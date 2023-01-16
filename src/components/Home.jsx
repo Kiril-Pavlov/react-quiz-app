6baaeb660383435e908e68bb376797f3da2769d6
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-16 21:46:59 +0100

    home page of quiz created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col gap-10 items-center justify-center w-full h-screen">
+      <h1 className="font-bold text-3xl">Kiril Quiz App</h1>
+      <div>Rules and description of quiz</div>
+      <input
+        type="text"
+        placeholder="Enter name here ..."
+        className="border-b-2 focus:outline-none focus:border-b-2 focus:border-black px-0 py-2"
+        id=""
+      />
+      <button className="bg-sky-500 px-10 py-3 rounded-full text-white font-semibold text-xl">
+        <Link to={'quiz'}>Start Quiz</Link>
+      </button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
