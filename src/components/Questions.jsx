0e00c205ce723ffe3fe4d8b6c5633f1328499879
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-17 20:10:07 +0100

    added some elements for question and answers and event flag event handler for it

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Questions = () => {
-  return (
-    <div>Questions</div>
-  )
-}
 
-export default Questions
+    const [selected, setSelected] = useState(undefined)
+
+    const handleRadioSelection = () =>{
+        setSelected(true)
+        console.log("radio buton changed")
+    }
+
+  return (
+    <div>
+      <h2>Question Placeholder 1</h2>
+
+      <ul>
+        <li>
+          <input type="radio" value={selected} name="options" id="q1-option" onChange={handleRadioSelection} />
+            <label htmlFor="q1-option">option</label>
+        </li>
+        <li>
+          <input type="radio" value={selected} name="options" id="q1-option" onChange={handleRadioSelection} />
+            <label htmlFor="q1-option">option</label>
+        </li>
+        <li>
+          <input type="radio" value={selected} name="options" id="q1-option" onChange={handleRadioSelection} />
+            <label htmlFor="q1-option">option</label>
+        </li>
+        <li>
+          <input type="radio" value={selected} name="options" id="q1-option" onChange={handleRadioSelection} />
+            <label htmlFor="q1-option">option</label>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Questions;
