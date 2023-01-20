d8cb265885ad8a052d8b4e101bc5edd1db2a32ce
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-20 21:54:27 +0100

    initial question reducer done

--- src/redux/questionReduser.jsx
+++ src/redux/questionReduser.jsx
@@ -1,1 +1,22 @@
-export const questionReducer = () => {}
+import { createSlice } from '@reduxjs/toolkit'
+
+export const questionReducer = createSlice({
+    name:'questions'
+    initialState : {
+        queue:[],
+        answers : [],
+        questionNumber : 0
+    },
+    reducers : {
+        startQuizAction : (state, action) => {
+            return {
+                ...state,
+                queue: action.payload
+            }
+        }
+    }
+})
+
+export const {startQuizAction} = questionReducer.actions
+
+export default questionReducer.reducer
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 //call reducers
-import { questionReducer } from './questionReduser'
+import questionReducer from './questionReduser'
 import { resultReducer } from './resultReducer'
+
 const rootReducer =  combineReducers({
     questions: questionReducer,
     results: resultReducer
 })
+
+//create store with reducer
+export default configureStore({reducer : rootReducer})
